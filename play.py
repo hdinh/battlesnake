9e5eb5fc64c1eb964726ab0e4f8df20c6417b9bc
Your Name <hdneto17@gmail.com>
2011-11-10 19:42:31 -0500

    battlesnake is born

---
+++ play.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+class Snake:
+    def __init__(self):
+        pass
+    def init(self):
+        print('cool')
+
+def main():
+    s = Snake()
+    s.init()
+
+if __name__ == '__main__':
+    main()
